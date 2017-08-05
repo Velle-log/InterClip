858e83a7c9441da1ad449de4bfae110a9496cce1
Saket Patel <saket.is.sam@gmail.com>
2017-08-05 06:14:33 +0000

    added clipboard and notifier

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # InterClip
-InterCLip is a utility which helps user/s to copy paste text (for now) across the Internet.  
+InterClip is a utility which helps user/s to copy paste text across the Internet.  
 It is being developed in `python3.6+` and uses `dropbox SDK`  
+
+## Requirements  
+Get a *dropbox token* from https://www.dropbox.com/developers  
+Install dependencies from **requirements.txt**  
+
+## How to use it?  
+  * Clone the repository: `git clone https://github.com/3Peers/InterClip.git`  
+  * Create a virtual environment: `virtualenv env -p python3.6`  
+  * Activate virtual environment: `source env/bin/activate`  
+  * Install requirements: `pip install -r requirements.txt`  
+  * ...
---
+++ helpers.py
@@ -0,0 +1,37 @@
+import pyperclip
+from PyQt5 import Qt
+import sys
+import time
+
+class ClipBoard:
+    """
+    Clipboard class holds the data copied or data
+    to be pasted on dropbox
+    """
+
+    def __init__(self):
+        self.state = None
+        self.notifier = Notify()
+
+    def push_text(self, text):
+        pyperclip.copy(text)
+        self.state = text
+        self.notifier.notify('Data Copied')
+
+    def pull_text(self):
+        self.notifier.notify('Data Pasted')
+        if self.state is not None:
+            return self.state
+        return pyperclip.paste()
+
+class Notifier:
+
+    def __init__(self):
+        self.application = Qt.QApplication(sys.argv)
+
+    def notify(self, text):
+        tray = Qt.QSystemTrayIcon(self.application)
+        tray.show()
+        tray.showMessage('InterClip Notification', text)
+        time.sleep(3)
+        tray.hide()
